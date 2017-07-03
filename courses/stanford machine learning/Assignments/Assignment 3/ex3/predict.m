06af9ffb62bd490b712646f726e322ef89ffc0c3
kamyarnazeri <kamyar.nzr@gmail.com>
2017-07-03 11:48:31 -0400

    Feedforward Propagation and Prediction

--- courses/stanford machine learning/Assignments/Assignment 3/ex3/predict.m
+++ courses/stanford machine learning/Assignments/Assignment 3/ex3/predict.m
@@ -3,33 +3,21 @@ function p = predict(Theta1, Theta2, X)
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+m = size(X, 1);                     % number of training examples
+n = size(X, 2);                     % number of features
+s2 = size(Theta1, 1);               % number of layer 2 units - Theta1: (s2 x n+1)
+k = size(Theta2, 1);                % number of labels - Theta2: (k x s2+1)
 
 
+a1 = [ones(m, 1) X];                % (m x n+1) matrix: add the intercept column
 
+z2 = a1 * Theta1';                  % (m x s2) matrix
+a2 = [ones(m, 1) sigmoid(z2)];      % (m x s2+1) matrix: apply sigmoid add the intercept column
 
+z3 = a2 * Theta2';                  % (m x k) matrix
+a3 = sigmoid(z3);                   % apply sigmoid
 
-
-
-
-
-% =========================================================================
-
+[M, p] = max(a3, [], 2);            % get index of the max element of each row
 
 end
