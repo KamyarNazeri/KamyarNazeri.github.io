5d2c49595abf224587a1d9e117ae13a2035d42d6
kamyarnazeri <kamyar.nzr@gmail.com>
2017-07-05 19:29:06 -0400

    reference google analytics over secured protocol

--- uoit/imaginglab/resources/site.js
+++ uoit/imaginglab/resources/site.js
@@ -34,7 +34,7 @@ var page = (function () {
                 (i[r].q = i[r].q || []).push(arguments)
             }, i[r].l = 1 * new Date(); a = s.createElement(o),
                 m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'http://www.google-analytics.com/analytics.js', 'ga');
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
         ga('create', 'UA-45081905-1', 'uoit.ca');
         ga('send', 'pageview');
