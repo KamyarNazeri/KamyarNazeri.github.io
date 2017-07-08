3e1faeceba8b2f09b037c1438f0543b4bf29ade9
kamyarnazeri <kamyar.nzr@gmail.com>
2017-07-08 16:21:19 -0400

    Regularized linear regression cost + gradient

--- uoit/courses/stanford machine learning/Assignments/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ uoit/courses/stanford machine learning/Assignments/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -5,33 +5,16 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 %   cost of using theta as the parameter for linear regression to fit the 
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+m = length(y);                              % number of training examples
+h = X * theta;                              % hypothesis
+theta(1) = 0;                               % not regularizing theta_0
+l = sum((h - y) .^ 2);                      % linear regression cost
+r = sum(theta .^ 2);                        % regularization (not regularizing theta_0)
+J = l / (2 * m) + lambda * r / (2 * m);     % cost function
 
 
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+% Regularized linear regression gradient
+grad = (X' * (h - y)) / m + lambda * theta / m; 
 grad = grad(:);
 
 end
