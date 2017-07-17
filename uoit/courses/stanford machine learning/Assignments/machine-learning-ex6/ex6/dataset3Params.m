503ddc0396550dd3396037c2ff86f6e03e7cf747
kamyarnazeri <kamyar.nzr@gmail.com>
2017-07-17 18:42:01 -0400

    ml assignment 6 find best SVM parameters

--- uoit/courses/stanford machine learning/Assignments/machine-learning-ex6/ex6/dataset3Params.m
+++ uoit/courses/stanford machine learning/Assignments/machine-learning-ex6/ex6/dataset3Params.m
@@ -7,28 +7,27 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+% Selected values of C and sigma
+params_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+best_error = 0;
+C = 0;
+sigma = 0;
 
+for c=params_vec
+    for s=params_vec
 
+        model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        
+        if best_error == 0 || error < best_error
+            best_error = error;
+            C = c;
+            sigma = s;
+        end
+    end
+end
 
-
-
-
-
-% =========================================================================
 
 end
